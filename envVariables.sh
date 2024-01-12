557f51e91a948b3423ce924dc49cb5e5f1645936
Ravi <rshah3@ebay.com>
2024-01-11 21:35:05 -0800

    update repo with detailed steps to build the application

---
+++ envVariables.sh
@@ -0,0 +1,10 @@
+export OPENAI_API_KEY=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
+export WEAVIATE_URL=https://rs-sandbox-cluster-pmnl9233.weaviate.network
+export WEAVIATE_API_KEY=opSuF1GZtvPmvXT81rFXliTOpenUkkKH69gs
+export RECORD_MANAGER_DB_URL=postgresql://postgres:n6NeRfJq_LuyL93@db.hdvlcsolvtiyehezxnjr.supabase.co:5432/postgres
+
+# for tracing
+export LANGCHAIN_TRACING_V2=false
+export LANGCHAIN_ENDPOINT=
+export LANGCHAIN_API_KEY=
+export LANGCHAIN_PROJECT=
---
+++ loadplatformData.py
@@ -0,0 +1,32 @@
+import os
+
+from langchain.document_loaders import TextLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+import os
+import uuid
+import ingest
+
+WEAVIATE_URL = os.environ["WEAVIATE_URL"]
+WEAVIATE_API_KEY = os.environ["WEAVIATE_API_KEY"]
+RECORD_MANAGER_DB_URL = os.environ["RECORD_MANAGER_DB_URL"]
+
+def load_platform_docs():
+    print("inside load_platform_docs")
+    documents = load_documents_from_files()
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=4000, chunk_overlap=200)
+    docs_transformed = text_splitter.split_documents(
+        documents
+    )
+    print(f"docs_transformed: {docs_transformed}")
+    ingest.ingest_docs_helper(docs_transformed)
+    
+
+def load_documents_from_files():
+    txt_files = ["./data/mphelp.txt"]
+    txt_loaders = [TextLoader(file_name) for file_name in txt_files]
+    documents = []
+    for loader in txt_loaders:
+       documents.extend(loader.load())
+    return documents
+
+load_platform_docs()
